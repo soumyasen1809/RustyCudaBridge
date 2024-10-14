17fb4d35e542a35c4c7ece41e183db64fcf3ba33
Soumya <soumyasen1809@gmail.com>
2024-10-14 21:44:04 +0200

    Continue adding primary context management

--- src/primary_context_management/cuda_device_primary_context.rs
+++ src/primary_context_management/cuda_device_primary_context.rs
@@ -1,7 +1,8 @@
 use libc::{c_int, c_uint};
 
 use crate::cuda_bindings::{
-    cuDevicePrimaryCtxGetState, cuDevicePrimaryCtxRelease, cudaError_t, CUdevice,
+    cuDevicePrimaryCtxGetState, cuDevicePrimaryCtxRelease, cuDevicePrimaryCtxReset,
+    cuDevicePrimaryCtxSetFlags, cudaError_t, CUdevice,
 };
 
 pub fn cuda_device_primary_ctx_get_state(
@@ -20,6 +21,24 @@ pub fn cuda_device_primary_ctx_get_state(
 
 pub fn cuda_device_primary_ctx_release(dev: CUdevice) -> Result<(), cudaError_t> {
     let result = unsafe { cuDevicePrimaryCtxRelease(dev) };
+
+    match result {
+        cudaError_t::cudaSuccess => Ok(()),
+        _ => Err(result),
+    }
+}
+
+pub fn cuda_device_primary_ctx_reset(dev: CUdevice) -> Result<(), cudaError_t> {
+    let result = unsafe { cuDevicePrimaryCtxReset(dev) };
+
+    match result {
+        cudaError_t::cudaSuccess => Ok(()),
+        _ => Err(result),
+    }
+}
+
+pub fn cuda_device_primary_ctx_set_flags(dev: CUdevice, flags: u32) -> Result<(), cudaError_t> {
+    let result = unsafe { cuDevicePrimaryCtxSetFlags(dev, flags as c_uint) };
 
     match result {
         cudaError_t::cudaSuccess => Ok(()),
@@ -55,5 +74,27 @@ mod tests {
 
         let result = cuda_device_primary_ctx_release(device);
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_cuda_primary_ctx_reset() {
+        let device: CUdevice = 0;
+
+        cuda_init(0).expect("Failed to initialize"); // actual flag passed as 0
+
+        let result = cuda_device_primary_ctx_reset(device);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_cuda_primary_ctx_set_flags() {
+        let flags: u32 = 1;
+        let device: CUdevice = 0;
+
+        cuda_init(0).expect("Failed to initialize"); // actual flag passed as 0
+
+        let result = cuda_device_primary_ctx_set_flags(device, flags);
+        assert!(result.is_ok());
+        assert_eq!(flags, 1); // flag changes to 1
     }
 }
